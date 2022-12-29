096fb4d30f6ab741f0c51e5f97d3701c9913e595
xWizzie <82777069+xWizzie@users.noreply.github.com>
2022-12-29 23:25:15 +0200

    I dont even know

--- Library/db_connect.php
+++ Library/db_connect.php
@@ -2,12 +2,12 @@
 
 session_start();
 
-$HOST = 'localhost';
+$HOST = 'localhost:3333';
 $DB_PWD = '';
 $USERNAME = 'root';
 $DATABASE = 'trydominoes';
-
-$conn = new mysqli($HOST, $USERNAME, $DB_PWD, $DATABASE);
+$PORT = '3333';
+$conn = new mysqli($HOST.':'.$POST, $USERNAME, $DB_PWD, $DATABASE);
 
 if ($conn->connect_error) {
     die('Connection Failed' . $conn->connect_error);
